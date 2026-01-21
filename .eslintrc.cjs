f5e3b3d6e1b05d29f10a2a9da428bc4d41196a63
詹银鑫 <zhanyinyin@clxkj.cn>
2026-01-21 11:32:49 +0800

    style: 修复 ESLint 配置中的逗号格式问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
     // es2021: true 表示支持ES2021语法特性
     es2021: true,
     // node: true 表示代码运行在Node.js环境中，支持Node.js全局变量如require、module等
-    node: true
+    node: true,
   },
   // extends 配置继承的规则集
   extends: [
@@ -21,14 +21,14 @@ module.exports = {
     // '@vue/eslint-config-typescript' 启用Vue的TypeScript配置
     '@vue/eslint-config-typescript',
     // '@vue/eslint-config-prettier' 禁用与Prettier冲突的ESLint规则
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
   ],
   // parserOptions 配置解析器选项
   parserOptions: {
     // ecmaVersion: 'latest' 使用最新的ECMAScript版本
     ecmaVersion: 'latest',
     // parser: '@typescript-eslint/parser' 使用TypeScript解析器
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   // plugins 配置使用的插件
   plugins: [
@@ -37,11 +37,11 @@ module.exports = {
     // '@typescript-eslint' TypeScript插件，用于检查TypeScript语法
     '@typescript-eslint',
     // 'prettier' Prettier插件，用于将Prettier错误作为ESLint错误显示
-    'prettier'
+    'prettier',
   ],
   // rules 配置具体的规则
   rules: {
     // 'prettier/prettier': 'error' 将Prettier格式化错误视为ESLint错误
-    'prettier/prettier': 'error'
-  }
-}
+    'prettier/prettier': 'error',
+  },
+};
